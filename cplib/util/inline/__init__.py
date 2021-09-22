06da23e16e2f50997cd54046a0719f856956499f
mcfx <git1@mcfx.us>
2021-09-22 14:19:40 +0800

    inliner: update py

--- cplib/util/inline/__init__.py
+++ cplib/util/inline/__init__.py
@@ -5,4 +5,4 @@ path = os.path.dirname(__file__)
 
 def get_lara_code():
     s = open(os.path.join(path, 'inliner.lara')).read()
-    return [(s[:s.find('aspectdef main')], 'inlineFunctions', 0, 990)]
+    return [(s[:s.find('aspectdef main')], 'inlineFunctions', 1, 1000)]  # must be last, since clava doesn't support using
